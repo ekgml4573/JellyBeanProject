51c0ffec985f66e845b10a89b78f18f0916f106c
dh4573 <dh4573@naver.com>
2023-07-10 17:18:18 +0900

    KDT과정별 DB 정보 조회(Json 반환)

--- src/main/java/com/jellybean/api/controller/KdtintroController.java
+++
@@ -1,8 +0,0 @@
-package com.jellybean.api.controller;
-
-public class KdtintroController {
-
-    public String test() {
-        return "Git 연동 test";
-    }
-}
--- src/main/java/com/jellybean/api/controller/TestController.java
+++ src/main/java/com/jellybean/api/controller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return "vvvvvv";
     }
 
 }
